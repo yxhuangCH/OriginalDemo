dbc2092247f0dbefd79a77537bcaad404f384e61
yxhuangCH <yxhuang2008@gmail.com>
2017-05-21 14:43:04 +0800

    add load more

---
+++ floatheader/src/main/java/com/yxhuang/floatheader/EndlessRecyclerOnScrollListener.java
@@ -0,0 +1,56 @@
+package com.yxhuang.floatheader;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Author: yxhuang
+ * Date: 2017/5/8
+ * Email: yxhuang@gmail.com
+ *  RecyclerView 上拉加载更多
+ */
+
+public abstract class EndlessRecyclerOnScrollListener extends RecyclerView.OnScrollListener {
+
+    private int previousTotal = 0;
+    private boolean loading = true;
+    int firstVisibleItem, visibleItemCount, totalItemCount;
+
+    private int currentPage = 0;
+
+    private LinearLayoutManager mLinearLayoutManager;
+
+    public EndlessRecyclerOnScrollListener(LinearLayoutManager linearLayoutManager) {
+        this.mLinearLayoutManager = linearLayoutManager;
+    }
+
+
+    public void reset(){
+        this.previousTotal = 0;
+        this.currentPage = 1;
+    }
+
+    @Override
+    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+
+        visibleItemCount = recyclerView.getChildCount();
+        totalItemCount = mLinearLayoutManager.getItemCount();
+        firstVisibleItem = mLinearLayoutManager.findFirstVisibleItemPosition();
+
+        if (loading) {
+            if (totalItemCount > previousTotal) {
+                loading = false;
+                previousTotal = totalItemCount;
+            }
+        }
+
+        if (!loading && (totalItemCount - visibleItemCount) <= firstVisibleItem) {
+            currentPage++;
+            onLoadMore(currentPage);
+            loading = true;
+        }
+    }
+
+    public abstract void onLoadMore(int currentPage);
+}
--- floatheader/src/main/java/com/yxhuang/floatheader/MainActivity.java
+++ floatheader/src/main/java/com/yxhuang/floatheader/MainActivity.java
@@ -1,10 +1,14 @@
 package com.yxhuang.floatheader;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,28 +18,56 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int FINISH_REFRESH = 1000;
+
     private HeaderAdapter mHeaderAdapter;
     private List<ItemBean> mList = new ArrayList<>(0);
     private RecyclerView mRecyclerView;
 
     private LinearLayout mHeaderView;
     private TextView tv_header;
+    private SwipeRefreshLayout swipe_layout;
+
+    private EndlessRecyclerOnScrollListener mEndlessRecyclerOnScrollListener;
+    private View mFooterView;
+    private HeaderViewRecyclerAdapter mHeaderViewRecyclerAdapter;
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == FINISH_REFRESH){
+                swipe_layout.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipe_layout.setRefreshing(false);
+                    }
+                });
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mHeaderView = (LinearLayout) findViewById(R.id.header_view);
         tv_header = (TextView) findViewById(R.id.tv_header);
+        swipe_layout = (SwipeRefreshLayout) findViewById(R.id.swipe_layout);
 
         final LinearLayoutManager manager = new LinearLayoutManager(this);
         manager.setOrientation(LinearLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(manager);
         mRecyclerView.addItemDecoration(new DividerItemDecoration(this, manager.getOrientation()));
         mHeaderAdapter = new HeaderAdapter(this, mList);
-        mRecyclerView.setAdapter(mHeaderAdapter);
+
+        mHeaderViewRecyclerAdapter = new HeaderViewRecyclerAdapter(mHeaderAdapter);
+        mFooterView = LayoutInflater.from(this).inflate(R.layout.view_footer, mRecyclerView, false);
+        mHeaderViewRecyclerAdapter.addFooterView(mFooterView);
+        mFooterView.setVisibility(View.GONE);
+
+        mRecyclerView.setAdapter(mHeaderViewRecyclerAdapter);
 
         // 头部置顶
         mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
@@ -68,17 +100,49 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        
         generateDate();
 
         updateFloatHeaderView(0);
+
+        swipe_layout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mHandler.sendEmptyMessageDelayed(FINISH_REFRESH, 3 * 1000);
+
+                // 1. 重置
+                mEndlessRecyclerOnScrollListener.reset();
+                // 2. 请求网络数据第一页
+                // TODO: 2017/5/21  网络请求第一页数据
+            }
+        });
+
+        mRecyclerView.addOnScrollListener(new EndlessRecyclerOnScrollListener(manager) {
+            @Override
+            public void onLoadMore(int currentPage) {
+                mFooterView.setVisibility(View.VISIBLE);
+
+                // TODO: 2017/5/21  请求数据
+                // 加载更多， 传 currentPage  进去
+
+                //弱网络情况下，数据不回来，则在 6后消失 footerView
+                mFooterView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (mFooterView.getVisibility() == View.VISIBLE){
+                            mFooterView.setVisibility(View.GONE);
+                        }
+                    }
+                }, 6 * 1000);
+            }
+        });
     }
 
 
     private void updateFloatHeaderView(int position){
         tv_header.setText(mList.get(position).getTime());
     }
-
-
+    
 
     private void generateDate(){
         String time1 = "20170519";
